d045f44643ef47bc514923bd9c0e7e7ff65e3793
Ido Salomon <idosalomon@gmail.com>
2026-07-11 12:43:15 +0300

    Add inclusive range selection

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { summarizeNumbers } from "../src/calculator.js";
+import { selectNumbersInRange, summarizeNumbers } from "../src/calculator.js";
 
 test("summarizes a number set", () => {
   assert.deepEqual(summarizeNumbers([3, 8, -2, 5]), {
@@ -23,5 +23,16 @@ test("rejects non-array input", () => {
   assert.throws(() => summarizeNumbers("1,2,3"), {
     name: "TypeError",
     message: "values must be an array",
+  });
+});
+
+test("selects values inside an inclusive range", () => {
+  assert.deepEqual(selectNumbersInRange([-4, 0, 3, 8, 12], 0, 8), [0, 3, 8]);
+});
+
+test("rejects an inverted range", () => {
+  assert.throws(() => selectNumbersInRange([1, 2, 3], 4, 2), {
+    name: "RangeError",
+    message: "minimum must not exceed maximum",
   });
 });
